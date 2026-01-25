7135f71ef9ed52e159a9051aed771af9af4da5a9
JavaCommons Technologies <javacommons@gmail.com>
2026-01-26 02:52:44 +0900

    .

--- test.sh
+++ test.sh
@@ -1,9 +1,12 @@
 #! /usr/bin/env bash
-set -uvx
+#set -uvx
 set -e
 cd "$(dirname "$0")"
 cwd=`pwd`
 ts=`date "+%Y.%m%d.%H%M.%S"`
 
-url-text `url-text https://github.com/my-homes/programs/raw/main/url.txt`
+#url-text `url-text https://github.com/my-homes/programs/raw/main/url.txt`
 url-text https://github.com/my-homes/programs/raw/main/version-url.txt
+line=`url-text https://github.com/my-homes/programs/raw/main/version-url.txt`
+echo "$line" | awk '{print $1}'
+echo "$line" | awk '{print $2}'
