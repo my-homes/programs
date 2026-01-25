56ec699a694e08d65df91dbf9b900ee9755812a7
JavaCommons Technologies <javacommons@gmail.com>
2026-01-26 02:36:26 +0900

    v2026.0126.0236.21

---
+++ release.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+set -uvx
+set -e
+cd "$(dirname "$0")"
+cwd=`pwd`
+ts=`date "+%Y.%m%d.%H%M.%S"`
+version=v$ts
+git-release-create $version abc.txt
+echo https://github.com/my-homes/programs/releases/download/$version/abc.txt>url.txt
+
+tag="$version"
+cd $cwd
+git add .
+git commit -m"$tag"
+git tag -a "$tag" -m"$tag"
+git push origin "$tag"
+git push origin HEAD:main
+git remote -v
