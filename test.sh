ea1847f385ba8e03d5c216c23294be99388a977d
JavaCommons Technologies <javacommons@gmail.com>
2026-01-26 13:10:58 +0900

    .

--- test.sh
+++ test.sh
@@ -1,13 +1,10 @@
 #! /usr/bin/env bash
-#set -uvx
+set -uvx
 set -e
 cd "$(dirname "$0")"
 cwd=`pwd`
 ts=`date "+%Y.%m%d.%H%M.%S"`
 
-#url-text `url-text https://github.com/my-homes/programs/raw/main/url.txt`
-url-text https://github.com/my-homes/programs/raw/main/version-url.txt
-line=`url-text https://github.com/my-homes/programs/raw/main/version-url.txt`
-version=`echo "$line" | awk '{print $1}'`
-url=`echo "$line" | awk '{print $2}'`
-url-download-2 tmp.$version.zip $url
+cd tmp
+find . -name "*.exe" -print | xargs -i dirname {} | xargs -i touch {}/.path
+find . -name .path
