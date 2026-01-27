c6099abc441c0d3da9949f7567202f54ba4de567
JavaCommons Technologies <javacommons@gmail.com>
2026-01-27 19:27:57 +0900

    .

--- mk-programs.sh
+++ mk-programs.sh
@@ -26,9 +26,6 @@ cp -rp /c/Users/user/scoop/apps/yt-dlp/current/* yt-dlp/
 mkdir -p ffmpeg
 cp -rp /c/Users/user/scoop/apps/ffmpeg/current/* ffmpeg/
 
-mkdir -p python
-cp -rp /c/Users/user/scoop/apps/python/current/* python/
-
 find . -name "*.exe" -print | xargs -i dirname {} | xargs -i touch {}/.path
 find . -name .path
 touch zulu17-jdk/.java_home
