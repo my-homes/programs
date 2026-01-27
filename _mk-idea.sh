1789f0769472b089e661ca33869c7b9e130567ac
JavaCommons Technologies <javacommons@gmail.com>
2026-01-27 23:29:00 +0900

    rider.v2026.0127.2329.00

--- _mk-idea.sh
+++ _mk-idea.sh
@@ -16,4 +16,4 @@ find . -mindepth 1 -maxdepth 1 | xargs -i cp -rpv {} $cwd/tmp/
 cd $cwd/tmp
 echo "./bin">.path-list.txt
 
-7z a -tzip -r idea.zip  $(find . -mindepth 1 -maxdepth 1)
+7z a -t7z -r idea.zip  $(find . -mindepth 1 -maxdepth 1)
--- _mk-rider.sh
+++ _mk-rider.sh
@@ -17,4 +17,4 @@ cd $cwd/tmp
 #touch bin/.path
 echo "./bin">.path-list.txt
 
-7z a -tzip -r rider.zip  $(find . -mindepth 1 -maxdepth 1)
+7z a -t7z -r rider.zip  $(find . -mindepth 1 -maxdepth 1)
