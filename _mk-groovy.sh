22cc8cdf57637a3ddc67380f5991650121733861
JavaCommons Technologies <javacommons@gmail.com>
2026-01-27 20:15:17 +0900

    groovy.v2026.0127.2015.16

---
+++ _mk-groovy.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+set -uvx
+set -e
+cd "$(dirname "$0")"
+cwd=`pwd`
+ts=`date "+%Y.%m%d.%H%M.%S"`
+
+scoop install git
+scoop update git
+
+cd $cwd
+rm -rf tmp
+mkdir tmp
+cd $cwd/tmp
+
+mkdir -p groovy
+cp -rp /c/Users/user/scoop/apps/groovy/current/* ./
+touch bin/.path
+
+7z a -tzip -r groovy.zip *
