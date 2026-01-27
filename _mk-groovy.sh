c73971c477e770e12fb6a7a871a4a1266c538e1d
JavaCommons Technologies <javacommons@gmail.com>
2026-01-27 23:10:53 +0900

    groovy.v2026.0127.2310.53

--- _mk-groovy.sh
+++ _mk-groovy.sh
@@ -15,6 +15,7 @@ cd $cwd/tmp
 
 mkdir -p groovy
 cp -rp /c/Users/user/scoop/apps/groovy/current/* ./
-touch bin/.path
+#touch bin/.path
+echo "./bin">.path-list.txt
 
-7z a -tzip -r groovy.zip *
+7z a -tzip -r groovy.zip  $(find . -mindepth 1 -maxdepth 1)
