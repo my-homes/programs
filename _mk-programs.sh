2b5f40cbdcc1b59c03eaa0f5d482fec6f5d96bbc
JavaCommons Technologies <javacommons@gmail.com>
2026-01-28 07:51:50 +0900

    programs.v2026.0128.0751.49

---
+++ _mk-programs.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/env bash
+#set -uvx
+set -e
+cd "$(dirname "$0")"
+cwd=`pwd`
+ts=`date "+%Y.%m%d.%H%M.%S"`
+
+scoop install git
+scoop bucket add extras
+scoop bucket add java
+
+while read -r app; do
+  echo $app
+  scoop install "$app"
+  scoop update "$app"
+done <<< $(cat $cwd/programs.txt)
+
+cd $cwd
+rm -rf tmp
+mkdir tmp
+cd $cwd/tmp
+
+while read -r app; do
+  echo $app
+  mkdir -p "$app"
+  cp -rp /c/Users/user/scoop/apps/$app/current/* ./$app/
+done <<< $(cat $cwd/programs.txt)
+
+find . -name "*.exe" -print | xargs -i dirname {} | xargs -i touch {}/.path
+touch .path-list.txt
+find . -name .path | xargs -i dirname {} >> .path-list.txt
+find . -name .path -exec rm -rf {} +
+
+7z a -tzip -r programs.zip $(find . -mindepth 1 -maxdepth 1)
