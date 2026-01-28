53891011ec338e5566a7f86b8bc304a5e8513ea2
JavaCommons Technologies <javacommons@gmail.com>
2026-01-28 09:30:08 +0900

    .

--- _mk-programs.sh
+++ _mk-programs.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 #set -uvx
-set -e
+#set -e
 cd "$(dirname "$0")"
 cwd=`pwd`
 ts=`date "+%Y.%m%d.%H%M.%S"`
@@ -16,16 +16,19 @@ while read -r app; do
 done <<< $(cat $cwd/programs.txt)
 
 cd $cwd
-rm -rf tmp
+mkdir -p tmp
+./rmdir.cmd tmp
 mkdir tmp
 cd $cwd/tmp
 
 while read -r app; do
   echo $app
   mkdir -p "$app"
-  cp -rp /c/Users/user/scoop/apps/$app/current/* ./$app/
+  #cp -rp /c/Users/user/scoop/apps/$app/current/* ./$app/
+  robocopy "C:/Users/user/scoop/apps/$app/current" ./$app -mir
 done <<< $(cat $cwd/programs.txt)
 
+cd $cwd/tmp
 find . -name "*.exe" -print | xargs -i dirname {} | xargs -i touch {}/.path
 touch .path-list.txt
 find . -name .path | xargs -i dirname {} >> .path-list.txt
--- mk
+++ mk
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 set -uvx
-set -e
+#set -e
 cd "$(dirname "$0")"
 cwd=`pwd`
 ts=`date "+%Y.%m%d.%H%M.%S"`
@@ -16,11 +16,18 @@ scoop install "$app"
 scoop update "$app"
 
 cd $cwd
-rm -rf tmp
+#rm -rf tmp
+#cmd.exe //c rd //s //q tmp
+#cmd.exe -c rd -s -q tmp
+mkdir -p tmp
+./rmdir.cmd tmp
 mkdir tmp
 cd $cwd/tmp
 
-cp -rp /c/Users/user/scoop/apps/$app/current/* ./
+#cp -rp /c/Users/user/scoop/apps/$app/current/* ./
+#xcopy /c/Users/user/scoop/apps/$app/current/* . -s -e
+#robocopy "`cygpath -w /c/Users/user/scoop/apps/$app/current`" . -mir
+robocopy "C:/Users/user/scoop/apps/$app/current" . -mir
 
 find . -name "*.exe" -print | xargs -i dirname {} | xargs -i touch {}/.path
 touch .path-list.txt
